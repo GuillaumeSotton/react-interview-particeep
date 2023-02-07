d3f173199dcffe80c62d0d81a873373ba7fbaf70
Guillaume Sotton <guillaume.sotton@preditic.com>
2023-02-07 12:29:55 +0100

    Remove dead code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 const Header = () => (
     <header>
-        <nav className="bg-white border-gray-200 px-4 py-2.5 dark:bg-gray-800">
+        <nav className="border-gray-200 px-4 py-2.5 bg-gray-800">
             <div className="flex items-center">
                 <a href="https://www.particeep.com/fr/" className="flex items-center">
                     <img src="/logo.png" className="h-6 mr-3 sm:h-9" alt="Logo"/>
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Particeep</span>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Particeep</span>
                 </a>
             </div>
         </nav>
